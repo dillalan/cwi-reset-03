9d2853ff9b96cc9bc4dd003296d734116f6508c3
alandill <alandill@rocketmail.com>
2021-10-04 18:16:48 -0300

    Adicionado saída de estoque

--- registradora-cheia-de-bugs/src/RelacaoPesoPreco.java
+++ registradora-cheia-de-bugs/src/RelacaoPesoPreco.java
@@ -5,22 +5,27 @@ public class RelacaoPesoPreco {
 
         if ("pao".equals(item)) {
             precoTotal = 12.75 * (qtd * 60 / 1000);
+            ItensPorQuantidade.setPao((ItensPorQuantidade.getPao()-qtd));
         }
 
         if ("torta".equals(item)) {
             precoTotal = 96.00 * ((double) qtd / (double) 16);
+            ItensPorQuantidade.setTorta((ItensPorQuantidade.getTorta()-qtd));
         }
 
         if ("leite".equals(item)) {
             precoTotal = 4.48 * qtd;
+            ItensPorQuantidade.setPao((ItensPorQuantidade.getPao()-qtd));
         }
 
         if ("cafe".equals(item)) {
             precoTotal = 9.56 * qtd;
+            ItensPorQuantidade.setLeite((ItensPorQuantidade.getLeite()-qtd));
         }
 
         if ("sanduiche".equals(item)) {
             precoTotal = 4.5 * qtd;
+            ItensPorQuantidade.setSanduiche((ItensPorQuantidade.getSanduiche()-qtd));
         }
 
         return precoTotal;
