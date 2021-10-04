5645409adb9d0523441bd92a0c64dea0756aeb67
alandill <alandill@rocketmail.com>
2021-10-04 20:19:29 -0300

    Corrige erro de venda sem estoque

--- registradora-cheia-de-bugs/src/RelacaoPesoPreco.java
+++ registradora-cheia-de-bugs/src/RelacaoPesoPreco.java
@@ -4,28 +4,58 @@ public class RelacaoPesoPreco {
         double precoTotal = 0;
 
         if ("pao".equals(item)) {
-            precoTotal = 12.75 * (qtd * 60 / 1000);
-            ItensPorQuantidade.setPao((ItensPorQuantidade.getPao()-qtd));
+            if (ItensPorQuantidade.getPao()<qtd){
+                System.out.println("Lamentamos, mas temos apenas "+ItensPorQuantidade.getPao()+" unidades, desculpe o transtorno!");
+                precoTotal = 12.75 * (ItensPorQuantidade.getPao() * (double) 60 / (double) 1000);
+                ItensPorQuantidade.setPao(0);
+            } else{
+                precoTotal = 12.75 * (qtd * (double) 60 / (double) 1000);
+                ItensPorQuantidade.setPao((ItensPorQuantidade.getPao()-qtd));
+            }
         }
 
         if ("torta".equals(item)) {
-            precoTotal = 96.00 * ((double) qtd / (double) 16);
-            ItensPorQuantidade.setTorta((ItensPorQuantidade.getTorta()-qtd));
+            if (ItensPorQuantidade.getTorta()<qtd){
+                System.out.println("Lamentamos, mas temos apenas "+ItensPorQuantidade.getTorta()+" unidades, desculpe o transtorno!");
+                precoTotal = 96.00 * ((double) ItensPorQuantidade.getTorta() / (double) 16);
+                ItensPorQuantidade.setTorta(0);
+            } else {
+                precoTotal = 96.00 * ((double) qtd / (double) 16);
+                ItensPorQuantidade.setTorta((ItensPorQuantidade.getTorta()-qtd));
+            }
         }
 
         if ("leite".equals(item)) {
-            precoTotal = 4.48 * qtd;
-            ItensPorQuantidade.setPao((ItensPorQuantidade.getPao()-qtd));
+            if (ItensPorQuantidade.getLeite()<qtd){
+                System.out.println("Lamentamos, mas temos apenas "+ItensPorQuantidade.getLeite()+" unidades, desculpe o transtorno!");
+                precoTotal = 4.48 * ItensPorQuantidade.getLeite();
+                ItensPorQuantidade.setLeite(0);
+            } else {
+                precoTotal = 4.48 * qtd;
+                ItensPorQuantidade.setLeite((ItensPorQuantidade.getLeite()-qtd));
+            }
         }
 
         if ("cafe".equals(item)) {
-            precoTotal = 9.56 * qtd;
-            ItensPorQuantidade.setLeite((ItensPorQuantidade.getLeite()-qtd));
+            if (ItensPorQuantidade.getCafe()<qtd){
+                System.out.println("Lamentamos, mas temos apenas "+ItensPorQuantidade.getCafe()+" unidades, desculpe o transtorno!");
+                precoTotal = 9.56 * ItensPorQuantidade.getCafe();
+                ItensPorQuantidade.setCafe(0);
+            } else{
+                precoTotal = 9.56 * qtd;
+                ItensPorQuantidade.setCafe((ItensPorQuantidade.getCafe()-qtd));
+            }
         }
 
         if ("sanduiche".equals(item)) {
-            precoTotal = 4.5 * qtd;
-            ItensPorQuantidade.setSanduiche((ItensPorQuantidade.getSanduiche()-qtd));
+            if (ItensPorQuantidade.getSanduiche()<qtd){
+                System.out.println("Lamentamos, mas temos apenas "+ItensPorQuantidade.getSanduiche()+" unidades, desculpe o transtorno!");
+                precoTotal = 4.5 * ItensPorQuantidade.getSanduiche();
+                ItensPorQuantidade.setSanduiche(0);
+            } else{
+                precoTotal = 4.5 * qtd;
+                ItensPorQuantidade.setSanduiche((ItensPorQuantidade.getSanduiche()-qtd));
+            }
         }
 
         return precoTotal;
