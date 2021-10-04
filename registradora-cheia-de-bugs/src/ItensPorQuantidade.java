1d3b0419941e28083c0dfb026b73047191dd047b
alandill <alandill@rocketmail.com>
2021-10-04 18:14:40 -0300

    Adicionado getters e setters para manipulação de itens

--- registradora-cheia-de-bugs/src/ItensPorQuantidade.java
+++ registradora-cheia-de-bugs/src/ItensPorQuantidade.java
@@ -6,5 +6,47 @@ public class ItensPorQuantidade {
     static int leite = 20;
     static int cafe = 20;
 
+    public static int getPao() {
+        return pao;
+    }
+
+    public static void setPao(int pao) {
+        ItensPorQuantidade.pao = pao;
+    }
+
+    public static int getTorta() {
+        return torta;
+    }
+
+    public static void setTorta(int torta) {
+        ItensPorQuantidade.torta = torta;
+    }
+
+    public static int getSanduiche() {
+        return sanduiche;
+    }
+
+    public static void setSanduiche(int sanduiche) {
+        ItensPorQuantidade.sanduiche = sanduiche;
+    }
+
+    public static int getLeite() {
+        return leite;
+    }
+
+    public static void setLeite(int leite) {
+        ItensPorQuantidade.leite = leite;
+    }
+
+    public static int getCafe() {
+        return cafe;
+    }
+
+    public static void setCafe(int cafe) {
+        ItensPorQuantidade.cafe = cafe;
+    }
+
+
+
 
 }
