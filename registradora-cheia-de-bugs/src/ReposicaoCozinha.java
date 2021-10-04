4ed779e32534895769f75a60e1f993c12a991b54
alandill <alandill@rocketmail.com>
2021-10-04 18:26:19 -0300

    Corrige ReposicaoCozinha. adiciona itens ao saldo de estoque.

--- registradora-cheia-de-bugs/src/ReposicaoCozinha.java
+++ registradora-cheia-de-bugs/src/ReposicaoCozinha.java
@@ -2,13 +2,13 @@ public class ReposicaoCozinha {
 
     static void reporItem(String item) {
         if ("paes".equals(item)) {
-            ItensPorQuantidade.pao = 3600;
+            ItensPorQuantidade.setPao(ItensPorQuantidade.getPao()+ 3600);
         }
         if ("torta".equals(item)) {
-            ItensPorQuantidade.torta = 4;
+            ItensPorQuantidade.setTorta(ItensPorQuantidade.getTorta()+4);
         }
         if ("sanduiche".equals(item)) {
-            ItensPorQuantidade.sanduiche = 20;
+            ItensPorQuantidade.setSanduiche(ItensPorQuantidade.getSanduiche()+20);
         }
     }
 }
