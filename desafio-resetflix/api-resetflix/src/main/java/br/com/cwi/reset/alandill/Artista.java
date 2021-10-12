d1ab931966fb50222399f7814aeab2a25cf370e4
alandill <alandill@rocketmail.com>
2021-10-12 16:07:24 -0300

    Cria classe artista e generaliza as Classes Ator e Diretor

---
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/alandill/Artista.java
@@ -0,0 +1,38 @@
+package br.com.cwi.reset.alandill;
+
+import java.time.LocalDate;
+
+public class Artista {
+    private Integer id;
+    private String nome;
+    private LocalDate dataNascimento;
+    private Integer anoInicioAtividade;
+
+    public Artista(String nome, LocalDate dataNascimento, Integer anoInicioAtividade) {
+        this.id = setId();
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.anoInicioAtividade = anoInicioAtividade;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public Integer getAnoInicioAtividade() {
+        return anoInicioAtividade;
+    }
+
+    public Integer setId(){
+        return this.id += 1;
+    }
+
+}
