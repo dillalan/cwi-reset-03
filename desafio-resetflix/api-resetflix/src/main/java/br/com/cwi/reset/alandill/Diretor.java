48a4d494cbb7f35d06a35e7654a8d99d8db67598
alandill <alandill@rocketmail.com>
2021-10-13 10:03:51 -0300

    Diretor herda a classe Artista

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/alandill/Diretor.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/alandill/Diretor.java
@@ -1,4 +1,11 @@
 package br.com.cwi.reset.alandill;
 
-public class Diretor {
+import java.time.LocalDate;
+
+public class Diretor extends Artista{
+
+    public Diretor(String nome, LocalDate dataNascimento, Integer anoInicioAtividade) {
+        super(nome, dataNascimento, anoInicioAtividade);
+    }
+
 }
