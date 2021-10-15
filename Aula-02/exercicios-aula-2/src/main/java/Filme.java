7ea2f8343d63580f56cca11b3242799fb90e7185
alandill <alandill@rocketmail.com>
2021-10-15 17:21:13 -0300

    Parte 4: Adiciona validação para a avaliação do filme. Joga exceção caso avaliação inválida

--- Aula-02/exercicios-aula-2/src/main/java/Filme.java
+++ Aula-02/exercicios-aula-2/src/main/java/Filme.java
@@ -6,13 +6,14 @@ public class Filme {
     private Double avaliacao;
     private Diretor diretor;
 
-    public Filme(String nome, String descricao, Integer duracao, Integer anoLancamento, Double avaliacao, Diretor diretor) {
+    public Filme(String nome, String descricao, Integer duracao, Integer anoLancamento, Double avaliacao, Diretor diretor) throws AvaliacaoForaDoPadraoException {
         this.nome = nome;
         this.descricao = descricao;
         this.duracao = duracao;
         this.anoLancamento = anoLancamento;
         this.avaliacao = avaliacao;
         this.diretor = diretor;
+        validarAvaliacao();
     }
 
     public void reproduzir(){
@@ -20,5 +21,11 @@ public class Filme {
         System.out.println("Descrição: "+this.descricao);
         System.out.println("Duração: "+this.duracao+" minutos");
         System.out.println("Diretor "+this.diretor.getNome());
+    }
+
+    private void validarAvaliacao() throws AvaliacaoForaDoPadraoException {
+        if (this.avaliacao > 5 || this.avaliacao < 1){
+            throw new AvaliacaoForaDoPadraoException("Erro! Parâmetro avaliacao deve ser maior que um e menor do que 5");
+        }
     }
 }
