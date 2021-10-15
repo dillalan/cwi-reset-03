54710ada6f7b8a96c4a8337bb2aa0045462a5d52
alandill <alandill@rocketmail.com>
2021-10-15 17:15:08 -0300

    Parte 4: Atributo idade removido. Atributo data de nascimento adicionado. Método de calcular idade criado.

--- Aula-02/exercicios-aula-2/src/main/java/Pessoa.java
+++ Aula-02/exercicios-aula-2/src/main/java/Pessoa.java
@@ -1,18 +1,24 @@
+import java.time.LocalDate;
+
 public class Pessoa {
     private String nome;
-    private Integer idade;
+    private LocalDate dataNascimento;
     private Genero genero;
 
-    public Pessoa(String nome, Integer idade, Genero genero) {
+    public Pessoa(String nome, LocalDate dataNascimento, Genero genero) {
         this.nome = nome;
-        this.idade = idade;
+        this.dataNascimento = dataNascimento;
         this.genero = genero;
     }
 
     public void perfil(){
         System.out.println("Nome: "+this.nome);
-        System.out.println("Idade: "+this.idade);
+        System.out.println("Idade: "+calcularIdade());
         System.out.println("Gênero: "+this.genero.getDescricao());
+    }
+
+    private Integer calcularIdade(){
+        return LocalDate.now().compareTo(this.dataNascimento);
     }
 
     public String getNome() {
