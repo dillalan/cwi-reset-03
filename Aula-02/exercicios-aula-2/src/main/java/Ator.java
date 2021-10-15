1e67b1096a6ea569e8ad46e23dd613934ed64dcf
alandill <alandill@rocketmail.com>
2021-10-15 17:18:14 -0300

    Altera o construtor para receber o tipo LocalDate para o parametro dataNascimento

--- Aula-02/exercicios-aula-2/src/main/java/Ator.java
+++ Aula-02/exercicios-aula-2/src/main/java/Ator.java
@@ -1,9 +1,11 @@
+import java.time.LocalDate;
+
 public class Ator extends Pessoa{
 
     private Integer oscarPremiado;
 
-    public Ator(String nome, Integer idade, Integer oscarPremiado, Genero genero) {
-        super(nome, idade, genero);
+    public Ator(String nome, LocalDate dataNascimento, Integer oscarPremiado, Genero genero) {
+        super(nome, dataNascimento, genero);
         this.oscarPremiado = oscarPremiado;
     }
 }
--- Aula-02/exercicios-aula-2/src/main/java/Diretor.java
+++ Aula-02/exercicios-aula-2/src/main/java/Diretor.java
@@ -1,9 +1,11 @@
+import java.time.LocalDate;
+
 public class Diretor extends Pessoa{
 
     private Integer filmesDirigidos;
 
-    public Diretor(String nome, Integer idade, Integer filmesDirigidos, Genero genero) {
-        super(nome, idade, genero);
+    public Diretor(String nome, LocalDate dataNascimento, Integer filmesDirigidos, Genero genero) {
+        super(nome, dataNascimento, genero);
 
         this.filmesDirigidos = filmesDirigidos;
     }
