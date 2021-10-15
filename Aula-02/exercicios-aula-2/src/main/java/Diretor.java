90533bb61680eedc53887e94afefa37c35ceaa9b
alandill <alandill@rocketmail.com>
2021-10-15 16:01:36 -0300

    Parte 2: Adiciona atributo gênero e método perfil

--- Aula-02/exercicios-aula-2/src/main/java/Diretor.java
+++ Aula-02/exercicios-aula-2/src/main/java/Diretor.java
@@ -2,14 +2,22 @@ public class Diretor {
     private String nome;
     private Integer idade;
     private Integer filmesDirigidos;
+    private Genero genero;
 
-    public Diretor(String nome, Integer idade, Integer filmesDirigidos) {
+    public Diretor(String nome, Integer idade, Integer filmesDirigidos, Genero genero) {
         this.nome = nome;
         this.idade = idade;
         this.filmesDirigidos = filmesDirigidos;
+        this.genero = genero;
     }
 
     public String getNome() {
         return nome;
+    }
+
+    public void perfil(){
+        System.out.println("Nome: "+this.nome);
+        System.out.println("Idade: "+this.idade);
+        System.out.println("Gênero: "+this.genero.getDescricao());
     }
 }
