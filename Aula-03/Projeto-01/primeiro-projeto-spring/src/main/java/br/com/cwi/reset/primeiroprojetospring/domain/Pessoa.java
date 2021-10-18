909b967d4ff6213d7d0f909dc22b6d92a64db6b9
alandill <alandill@rocketmail.com>
2021-10-18 16:25:23 -0300

    Adicionando getters e setters

--- Aula-03/Projeto-01/primeiro-projeto-spring/src/main/java/br/com/cwi/reset/primeiroprojetospring/domain/Pessoa.java
+++ Aula-03/Projeto-01/primeiro-projeto-spring/src/main/java/br/com/cwi/reset/primeiroprojetospring/domain/Pessoa.java
@@ -13,17 +13,33 @@ public class Pessoa {
         this.genero = genero;
     }
 
-    public void perfil(){
-        System.out.println("Nome: "+this.nome);
-        System.out.println("Idade: "+calcularIdade());
-        System.out.println("Gênero: "+this.genero.getDescricao());
+    public void perfil() {
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Idade: " + calcularIdade());
+        System.out.println("Gênero: " + this.genero.getDescricao());
     }
 
-    private Integer calcularIdade(){
+    private Integer calcularIdade() {
         return LocalDate.now().compareTo(this.dataNascimento);
     }
 
     public String getNome() {
         return nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Genero getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Genero genero) {
+        this.genero = genero;
     }
 }
