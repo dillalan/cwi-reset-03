9f4f84cd4eba52700422040a34f8d5aaf8b699f4
alandill <alandill@rocketmail.com>
2021-10-19 17:25:13 -0300

    Métodos de criarAtor, consultarAtoresEmAtividade, consultarAtores e consultarAtor

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/alandill/controller/AtorController.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/alandill/controller/AtorController.java
@@ -2,14 +2,14 @@ package br.com.cwi.reset.alandill.controller;
 
 import br.com.cwi.reset.alandill.FakeDatabase;
 import br.com.cwi.reset.alandill.domain.Ator;
-import br.com.cwi.reset.alandill.exception.NaoEncontradoException;
-import br.com.cwi.reset.alandill.exception.NomeException;
-import br.com.cwi.reset.alandill.exception.TemporalException;
+import br.com.cwi.reset.alandill.exception.*;
 import br.com.cwi.reset.alandill.request.AtorRequest;
+import br.com.cwi.reset.alandill.response.AtorEmAtividade;
 import br.com.cwi.reset.alandill.service.AtorService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RequestMapping("/atores")
@@ -24,12 +24,26 @@ public class AtorController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public void criarAtor(@RequestBody AtorRequest atorRequest) throws TemporalException, NomeException {
+    public void criarAtor(@RequestBody AtorRequest atorRequest) throws TemporalException, NomeException, ObrigatorioException {
         atorService.criarAtor(atorRequest);
     }
 
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/em_atividade")
+    public List<AtorEmAtividade> consultarAtoresEmAtividade(@PathParam("{filtroNome}") String filtroNome) throws SemCadastroException, NaoEncontradoException {
+        return atorService.listarAtoresEmAtividade(filtroNome);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{id}")
+    public Ator consultarAtor(@PathVariable Integer id) throws NaoEncontradoException, ObrigatorioException {
+        return atorService.consultarAtor(id);
+
+    }
+
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping
-    public List<Ator> consultarAtores(){
-        return FakeDatabase.getInstance().recuperaAtores();
+    public List<Ator> consultarAtores() throws SemCadastroException {
+        return atorService.consultarAtores();
     }
 }
