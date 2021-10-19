ae822d731058ceceb5dc3e8e24c722e3a71d91a6
alandill <alandill@rocketmail.com>
2021-10-19 09:35:44 -0300

    Adiciona a classe EstudioService

---
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/alandill/service/EstudioService.java
@@ -0,0 +1,12 @@
+package br.com.cwi.reset.alandill.service;
+
+import br.com.cwi.reset.alandill.FakeDatabase;
+
+public class EstudioService {
+
+    private FakeDatabase fakeDatabase;
+
+    public EstudioService(FakeDatabase fakeDatabase){
+        this.fakeDatabase = fakeDatabase;
+    }
+}
