ecce8cc2ca1943c793a106c32ae3d7510f0e9824
alandill <alandill@rocketmail.com>
2021-10-20 08:11:05 -0300

    Corrige listarDiretores() e consultarDiretor, elimina imprimePerfil e sobrecargas

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/alandill/service/DiretorService.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/alandill/service/DiretorService.java
@@ -12,6 +12,7 @@ import java.util.List;
 public class DiretorService {
     private FakeDatabase fakeDatabase;
     private Integer incremento = 1;
+
     public DiretorService(FakeDatabase fakeDatabase) {
         this.fakeDatabase = fakeDatabase;
     }
@@ -24,24 +25,21 @@ public class DiretorService {
         this.incremento += 1;
     }
 
-    public void imprimePerfil(Diretor diretor){
-        System.out.println("Nome: "+diretor.getNome());
-        System.out.println("Data de Nascimento: "+diretor.getDataNascimento());
-        System.out.println("Ano de início de atividade: "+diretor.getAnoInicioAtividade());
-    }
-
-    public void cadastrarDiretor(DiretorRequest diretorRequest) throws NomeException, TemporalException {
-        if (diretorRequest.getNome().split(" ").length<2){
+    public void cadastrarDiretor(DiretorRequest diretorRequest) throws NomeException, TemporalException, ObrigatorioException {
+        if (diretorRequest.getNome().isEmpty()) {
+            throw new ObrigatorioException("Campo obrigatório não informado. Favor informar o " +
+                    "campo {DiretorRequest diretorRequest}.");
+        } else if (diretorRequest.getNome().split(" ").length < 2) {
             throw new NomeException("Deve ser informado no mínimo nome e sobrenome para o diretor.");
-        } else if (diretorRequest.getDataNascimento().isAfter(LocalDate.now())){
+        } else if (diretorRequest.getDataNascimento().isAfter(LocalDate.now())) {
             throw new TemporalException("Não é possível cadastrar diretores não nascidos.");
-        } else if (diretorRequest.getAnoInicioAtividade()<diretorRequest.getDataNascimento().getYear()){
+        } else if (diretorRequest.getAnoInicioAtividade() < diretorRequest.getDataNascimento().getYear()) {
             throw new TemporalException("Ano de início de atividade inválido para o diretor cadastrado.");
         }
         for (Diretor diretor :
                 this.fakeDatabase.recuperaDiretores()) {
-            if (diretor.getNome().equals(diretorRequest.getNome())){
-                throw new NomeException("Já existe um diretor cadastrado para o nome {"+diretorRequest.getNome()+"}.");
+            if (diretor.getNome().equals(diretorRequest.getNome())) {
+                throw new NomeException("Já existe um diretor cadastrado para o nome {" + diretorRequest.getNome() + "}.");
             }
         }
         diretorRequest.setId(getIncremento());
@@ -49,46 +47,40 @@ public class DiretorService {
         setIncremento();
     }
 
-    public void cadastrarDiretor() throws ObrigatorioException {
-        throw new ObrigatorioException("Campo obrigatório não informado. Favor informar o campo {DiretorRequest diretorRequest}.");
-    }
-
     public List<Diretor> listarDiretores(String filtroNome) throws SemCadastroException, NaoEncontradoException {
+
+        List<Diretor> diretorFiltrado = new ArrayList<>();
+
         if (this.fakeDatabase.recuperaDiretores().isEmpty()) {
             throw new SemCadastroException("Nenhum diretor cadastrado, favor cadastrar diretores.");
-        }
-        List <Diretor> diretorFiltrado = new ArrayList<>();
-        String stringDoFiltro = filtroNome.toLowerCase();
-        String stringDaDatabase;
-        boolean flag = false;
-        for (Diretor diretor:
-             this.fakeDatabase.recuperaDiretores()) {
-            stringDaDatabase = diretor.getNome().toLowerCase();
-            if (stringDaDatabase.contains(stringDoFiltro)){
-                diretorFiltrado.add(diretor);
-                flag = true;
+        } else if (filtroNome == null) {
+            return this.fakeDatabase.recuperaDiretores();
+        } else {
+            String stringDoFiltro = filtroNome.toLowerCase();
+            String stringDaDatabase;
+            boolean flag = false;
+
+            for (Diretor diretor :
+                    this.fakeDatabase.recuperaDiretores()) {
+                stringDaDatabase = diretor.getNome().toLowerCase();
+                if (stringDaDatabase.contains(stringDoFiltro)) {
+                    diretorFiltrado.add(diretor);
+                    flag = true;
+                }
             }
-        }
-        if(!flag){
-            throw new NaoEncontradoException("Diretor não encontrado com o filtro {"+filtroNome+"}, favor informar " +
-                    "outro filtro.");
+            if (!flag) {
+                throw new NaoEncontradoException("Diretor não encontrado com o filtro {" + filtroNome + "}, favor informar " +
+                        "outro filtro.");
+            }
         }
         return diretorFiltrado;
     }
 
-    public List<Diretor> listarDiretores() throws SemCadastroException {
-        if (this.fakeDatabase.recuperaDiretores().isEmpty()) {
-            throw new SemCadastroException("Nenhum diretor cadastrado, favor cadastrar diretores.");
-        }
-        return this.fakeDatabase.recuperaDiretores();
-    }
-
-
     public Diretor consultarDiretor(Integer id) throws NaoEncontradoException, ObrigatorioException {
-        Diretor diretorConsultado = new Diretor("", LocalDate.of(2019,11,5), 2019);
+        Diretor diretorConsultado = new Diretor("", LocalDate.of(2019, 11, 5), 2019);
         boolean flag = false;
 
-        if (id==null){
+        if (id == null) {
             throw new ObrigatorioException("Campo obrigatório não informado. Favor informar o campo {Integer id}.");
         }
         for (Diretor diretor :
@@ -99,9 +91,9 @@ public class DiretorService {
                 break;
             }
         }
-        if (!flag){
-            throw new NaoEncontradoException("Nenhum diretor encontrado com o parâmetro id={"+id+"}, favor verifique os parâmetros informados.");
+        if (!flag) {
+            throw new NaoEncontradoException("Nenhum diretor encontrado com o parâmetro id={" + id + "}, favor verifique os parâmetros informados.");
         }
-    return diretorConsultado;
+        return diretorConsultado;
     }
 }
