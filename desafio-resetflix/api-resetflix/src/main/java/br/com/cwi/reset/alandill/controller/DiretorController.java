a5b7898945d7d4bbd5ca62c62fa7c12bc3be535c
alandill <alandill@rocketmail.com>
2021-10-20 08:04:51 -0300

    Adiciona métodos cadastraDiretor(), listarDiretores() e consultarDiretor()

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/alandill/controller/DiretorController.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/alandill/controller/DiretorController.java
@@ -2,19 +2,19 @@ package br.com.cwi.reset.alandill.controller;
 
 import br.com.cwi.reset.alandill.FakeDatabase;
 import br.com.cwi.reset.alandill.domain.Diretor;
-import br.com.cwi.reset.alandill.exception.NaoEncontradoException;
-import br.com.cwi.reset.alandill.exception.NomeException;
-import br.com.cwi.reset.alandill.exception.SemCadastroException;
-import br.com.cwi.reset.alandill.exception.TemporalException;
+import br.com.cwi.reset.alandill.exception.*;
 import br.com.cwi.reset.alandill.request.DiretorRequest;
 import br.com.cwi.reset.alandill.service.DiretorService;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RequestMapping("/diretores")
 @RestController
+@Controller
 public class DiretorController {
 
     private DiretorService diretorService;
@@ -25,14 +25,19 @@ public class DiretorController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public void cadastrarDiretor(DiretorRequest diretorRequest) throws TemporalException, NomeException {
+    public void cadastrarDiretor(@RequestBody DiretorRequest diretorRequest) throws TemporalException, NomeException, ObrigatorioException {
         diretorService.cadastrarDiretor(diretorRequest);
     }
 
-
-    public List <Diretor> listarDiretores(String filtroNome) throws NaoEncontradoException, SemCadastroException {
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping
+    public List <Diretor> listarDiretores(@PathParam("{filtroNome}") String filtroNome) throws NaoEncontradoException, SemCadastroException {
         return diretorService.listarDiretores(filtroNome);
     }
 
-
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{id}")
+    public Diretor consultarDiretor(@PathVariable Integer id) throws NaoEncontradoException, ObrigatorioException {
+        return diretorService.consultarDiretor(id);
+    }
 }
