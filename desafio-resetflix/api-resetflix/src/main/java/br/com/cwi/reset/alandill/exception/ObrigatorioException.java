a861e04766f99fa7c9cb3eea38317e71a1c8291a
alandill <alandill@rocketmail.com>
2021-10-20 14:55:46 -0300

    Adiciona ResponseStatus

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/alandill/exception/ObrigatorioException.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/alandill/exception/ObrigatorioException.java
@@ -1,5 +1,9 @@
 package br.com.cwi.reset.alandill.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ObrigatorioException extends Exception{
     public ObrigatorioException(String message) {
         super(message);
--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/alandill/exception/TemporalException.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/alandill/exception/TemporalException.java
@@ -1,5 +1,9 @@
 package br.com.cwi.reset.alandill.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class TemporalException extends Exception{
     public TemporalException(String message) {
         super(message);
